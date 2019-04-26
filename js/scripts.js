5755b0cab82dd11ba56bfa4172d8b64f96a8b6f4
keitmark <keitmark@mail.ru>
2019-04-26 15:40:01 +0400

    feat: add catalog page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
+
 $(document).ready(function(){
+
+  //fixed menu
+
+    $(window).scroll(function(){
+      
+      if($(window).scrollTop() >= $('.header-top').height()){
+        $('.navigation').addClass('navigation--fixed');
+      }
+      else {
+        $('.navigation').removeClass('navigation--fixed');
+      }
+    });
+   //open search
+
+   $('.header-search__search-icon').click(function(){
+    $('.header-search').addClass('header-search--opened');
+  });
+
+  $('.header-search__form-close').click(function(){
+    $('.header-search').removeClass('header-search--opened');
+  });
+
    
     $("#to_top").click(function(){
         $("html, body").animate({scrollTop:0},500);
@@ -13,6 +36,23 @@ $(document).ready(function(){
         width: 'resolve'
      });
 
+     $('.init-select').select2({
+      width: 'resolve'
+   });
+
+   $(".range-slider").ionRangeSlider({
+      min: 0,
+      max: 50000,
+      from: 0,
+      type: "double",
+      hide_min_max: true,
+      postfix: " р",
+      grid: false,
+      grid_num: 5,
+      onChange: function(){
+        
+      }
+  });
      
     
     //  $('.tel-inp').inputmask({
@@ -38,5 +78,51 @@ $(document).ready(function(){
               }
             }
         ]
+      });
+
+      $('.filter__button-show').click(function(){
+        $(this).addClass('filter__button-show--hidden');
+        $(this).parent().find('.filter__hidden-checkboxes').addClass('filter__hidden-checkboxes--visible');
+      });
+
+      $('.filter__button-refresh').click(function(){
+        $('.one-checkbox__checkbox').prop('checked',false);
+      });
+
+      // open/close filter
+
+      function openFilter() {
+        $('.catalog__filter').addClass('catalog__filter--visible');
+        $('body, html').addClass('no-scroll');
+      }
+      function closeFilter() {
+        if($('.catalog__filter').hasClass('catalog__filter--visible')){
+          $('.catalog__filter').removeClass('catalog__filter--visible');
+          $('body, html').removeClass('no-scroll');
+        }
+      }
+
+      $('.xs-filter-button').click(function(e){
+        e.stopPropagation();
+
+        if($('.catalog__filter').hasClass('catalog__filter--visible')){
+          closeFilter();
+        }
+        else {
+          openFilter();
+        }
+        
+      });
+
+      $('body,html').click(function(){
+        closeFilter();
+      })
+
+      $('.catalog__filter').click(function(e){
+        e.stopPropagation();
+      });
+
+      $('.catalog__xs-close-filter').click(function(){
+        closeFilter();
       });
 });
